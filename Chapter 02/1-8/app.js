f07d1161c3171cca6da38240491d7210988752d5
mshahrukhrao <26709224+mshahrukhrao@users.noreply.github.com>
2024-03-09 02:14:01 +0500

    Add files via upload

---
+++ Chapter 02/1-8/app.js
@@ -0,0 +1,37 @@
+var username; //1
+var myName = "Shahrukh Rao"; //2
+var message ="Hello World"; //3
+
+alert(message); //3
+
+var age = 15; //4
+var course = "Certified Mobile Application Development"; //4
+
+alert(myName); //4
+alert(age+" years old"); //4
+alert(course); //4
+
+var pizza ="PIZZA \n PIZZ \n PIZ \n PI \n P"; //5
+alert(pizza); //5
+
+var email = 'My email address is'+" shahrukh.rao.x@gmail.com"; //6
+alert(email); //6
+
+var book = "I'm trying to learn from the book"+" A smarter way to learn JavaScript"; //7
+alert(book); //7
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
